3452b08b3d7071cece220cead93c02b90d3a4671
24448mo-lab <24448mo@hvhs.school.nz>
2026-06-10 01:00:05 +0000

    Jun 10, 2026, 1:00 AM

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!")
+let randomName = "Lukys";
+let randomAge = 16;
+let randomMoney = 60; 
+let actualYear = 2026; 
+
+console.log("Hi " + randomName);
+console.log("As of " + actualYear + " you are " + randomAge);
+console.log ("You were born in " + (actualYear - randomAge));
+console.log ("In 10 years you will be " + (randomAge + 10) + " years old");
+console.log ("In 10 years you will be " + (randomAge + 10) + " years old");
+console.log("You have " + randomMoney);
+console.log ("You spend half of your money, now you have " + (randomMoney / 2));
+console.log ("Then you get $3, now you have " + (randomMoney / 2 + 3));
