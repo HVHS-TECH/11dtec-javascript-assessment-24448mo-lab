f948eaa28592a53beac6f70f42cdbad5bb72ba22
24448mo-lab <24448mo@hvhs.school.nz>
2026-06-28 21:44:22 +0000

    Jun 28, 2026, 9:44 PM

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ console.log("Hello world!")
 
 alert("Welcome to my first Javascript program!")
 
-var userName = prompt(“What is your name?”, “???”)
+//var userName = prompt(“What is your name?”, “???”)
 
-prompt ("Welcome to this website! \n Please enter ur name")
+//prompt ("Welcome to this website! \n Please enter ur name")
