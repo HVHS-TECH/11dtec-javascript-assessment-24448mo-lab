8f00c579ee8893a19abd730a4090624bd9d31634
24448mo-lab <24448mo@hvhs.school.nz>
2026-06-29 22:49:48 +0000

    Jun 29, 2026, 10:49 PM

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ var userName = prompt ("Welcome to this website! \n Please enter ur name")
 
 console.log("Hello, " + userName + "!")
 
+var userAge = prompt ("How old are you?")
+
+console.log("You are " + userAge + " years old!")
+
+var userColor = prompt ("What is your favorite color?")
+
+console.log("Your favorite color is " + userColor + "!")
+
+alert("Thank you for visiting my website, " + userName + "!")           
