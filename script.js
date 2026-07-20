fcb820d122ed2fa491db237c178f9ff56ed20be3
24448mo-lab <24448mo@hvhs.school.nz>
2026-07-20 22:39:59 +0000

    Jul 20, 2026, 10:39 PM

--- script.js
+++ script.js
@@ -14,12 +14,4 @@ console.log("You are " + userAge + " years old!")
 
 var userFood = prompt ("What are you choosing today? 1. Sprite ($2) 2. Burger ($5) 3. Cake ($3)")
 
-if (userFood == "1") {
-    console.log("You have chosen Sprite. Your total is $2.")
-} else if (userFood == "2") {
-    console.log("You have chosen Burger. Your total is $5.")
-} else if (userFood == "3") {
-    console.log("You have chosen Cake. Your total is $3.")
-}
-
 alert("Thank you for visiting my website, " + userName + "!")     
